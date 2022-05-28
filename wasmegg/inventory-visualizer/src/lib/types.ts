821c5224f405db3f941a674f168abb30587f3276
Philip (flip) Kromer <flip@mrflip.com>
2022-05-28 14:42:04 -0500

    feat: sorting within kinds, sort ordering

--- wasmegg/inventory-visualizer/src/lib/types.ts
+++ wasmegg/inventory-visualizer/src/lib/types.ts
@@ -1,3 +1,12 @@
 //
-export interface Orderable  { id: number, name: string, weight: number }
+export interface Orderable  { name: string, weight: number, img: string, glyph: string }
 export interface Orderables { [key: string]: Orderable }
+
+export interface LayoutOrderables {
+  kinds:      Orderables
+  artifacts:  Orderables
+  stones:     Orderables
+  aspects:    Orderables
+}
+
+export type LayoutAxis = keyof LayoutOrderables
