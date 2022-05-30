8b8bebce8d3e7b3a1e86df4dffe6071eec5bcf3f
Philip (flip) Kromer <flip@mrflip.com>
2022-05-30 01:34:25 -0500

    chore: addressed typescript/rollup complaints with new tbd packages

--- wasmegg/_home/vite.config.ts
+++ wasmegg/_home/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
       ui: path.resolve(__dirname, '../../ui'),
+      lib: path.resolve(__dirname, '../../lib'),
     },
   },
   plugins: [vue()],
