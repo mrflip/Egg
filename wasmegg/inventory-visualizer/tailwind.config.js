7275c9499c6d0b7b87c9fc7b7fbbf6321346c294
Philip (flip) Kromer <flip@mrflip.com>
2022-06-04 14:07:04 -0500

    chore: tickmarks less likely to get out of control, bookmark moved up and stone-or-frag is now a non-fancy item

--- wasmegg/inventory-visualizer/tailwind.config.js
+++ wasmegg/inventory-visualizer/tailwind.config.js
@@ -2,4 +2,14 @@ module.exports = {
   mode: 'jit',
   content: ['./index.html', './src/**/*.{vue,ts}', '../../ui/**/*.vue'],
   plugins: [require('@tailwindcss/forms')],
+  theme: {
+    screens: {
+      'xs':   '440px',
+      'sm':   '640px',
+      'md':   '768px',
+      'lg':  '1024px',
+      'xl':  '1280px',
+      '2xl': '1536px',
+    },
+  },
 };
